cc242219a4224812bcd14841d86b75fb72989eb0
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-10 13:43:25 -0600

    save1

--- classes.js
+++ classes.js
@@ -30,6 +30,18 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age 
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+
 
 
 ////////// PROBLEM 2 //////////
